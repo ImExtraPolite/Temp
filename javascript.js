d3e24667b52f7c9fcf8ff092b2b7f21cca446adc
ImExtraPolite <jaleel1666@gmail.com>
2023-06-15 16:25:55 -0400

    make rows and columns

--- javascript.js
+++ javascript.js
@@ -1,4 +1,24 @@
-const container = document.querySelector("#container");
-const squares = document.createElement("div");
+function columns() {
+  for (let i = 1; i <= 16; i++) {
+    const container = document.querySelector("#container");
+    const squares = document.createElement("div");
 
-container.appendChild(squares);
+    container.appendChild(squares);
+  }
+}
+
+function rows() {
+  for (let i = 1; i <= 16; i++) {
+    const container = document.querySelector("#container");
+    const squares = document.createElement("div");
+
+    container.appendChild(squares);
+  }
+}
+
+
+
+
+
+columns();
+rows();
