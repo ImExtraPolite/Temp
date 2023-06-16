6f5ad7ab2116991f0d5b8ce780857e18a63b40dc
ImExtraPolite <jaleel1666@gmail.com>
2023-06-16 00:04:05 -0400

    grid done

--- javascript.js
+++ javascript.js
@@ -2,6 +2,9 @@ function columns() {
   for (let i = 1; i <= 16; i++) {
     const container = document.querySelector("#container");
     const squares = document.createElement("div");
+    squares.classList.add("row" + i);
+    squares.style.cssText = "display: flex;";
+
 
     container.appendChild(squares);
   }
@@ -9,16 +12,15 @@ function columns() {
 
 function rows() {
   for (let i = 1; i <= 16; i++) {
-    const container = document.querySelector("#container");
-    const squares = document.createElement("div");
+    for (let j = 1; j <= 16; j++) {
+      const rows = document.querySelector(".row" + i);
+      const squares = document.createElement("div");
+      squares.style.cssText = "border: solid black; padding: 20px;";
 
-    container.appendChild(squares);
+      rows.appendChild(squares);
+    }
   }
 }
-
-
-
-
 
 columns();
 rows();
