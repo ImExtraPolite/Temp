d29c489b2fe1197bd6b9c94bd9444ce20a3a83f9
ImExtraPolite <jaleel1666@gmail.com>
2023-06-18 17:37:19 -0400

    make calculator

--- javascript.js
+++ javascript.js
@@ -26,6 +26,10 @@ function operate(num1, operation, num2) {
   }
 }
 
+function refresh() {
+  location.reload();
+}
+
 // let num1 = Number(prompt("Enter first number: "));
 // let num2 = Number(prompt("Enter second number: "));
 // let operation = prompt("Enter operation: ");
