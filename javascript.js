30ce90124b10672a2dd9945e4ed7c36cc64f9d05
ImExtraPolite <jaleel1666@gmail.com>
2023-06-19 16:16:23 -0400

    make display work

--- javascript.js
+++ javascript.js
@@ -15,6 +15,12 @@ function divide(num1, num2) {
 }
 
 function operate(num1, operation, num2) {
+  const equal = document.querySelector("#equal");
+
+  equal.addEventListener("click", () => {
+
+  });
+
   if (operation == "+") {
     return add(num1, num2);
   } else if (operation == "-") {
@@ -29,6 +35,22 @@ function operate(num1, operation, num2) {
 function refresh() {
   location.reload();
 }
+
+let num1 = "";
+let expression = "";
+const display = document.querySelector("#display");
+const fnum = document.querySelector("button");
+
+fnum.addEventListener("click", () => {
+  num1 = fnum.textContent;
+  expression = expression + num1;
+  display.textContent = expression;
+
+  console.log(num1);
+  console.log(expression);
+  console.log(display.textContent);
+})
+
 
 // let num1 = Number(prompt("Enter first number: "));
 // let num2 = Number(prompt("Enter second number: "));
