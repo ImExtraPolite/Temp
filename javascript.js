133136a9d814387987c9f49c00fc31f335df4dca
ImExtraPolite <jaleel1666@gmail.com>
2023-06-19 16:47:07 -0400

    get display to work

--- javascript.js
+++ javascript.js
@@ -18,42 +18,78 @@ function operate(num1, operation, num2) {
   const equal = document.querySelector("#equal");
 
   equal.addEventListener("click", () => {
+    if (operation == "+") {
+      const display = document.querySelector("#display");
 
+      display.textContent = add(num1, num2);
+      // return add(num1, num2);
+    } else if (operation == "-") {
+      const display = document.querySelector("#display");
+
+      display.textContent = subtract(num1, num2);
+      // return subtract(num1, num2);
+    } else if (operation == "*") {
+      const display = document.querySelector("#display");
+
+      display.textContent = multiply(num1, num2);
+      // return multiply(num1, num2);
+    } else if (operation == "/") {
+      const display = document.querySelector("#display");
+
+      display.textContent = divide(num1, num2);
+      // return divide(num1, num2);
+    }
   });
-
-  if (operation == "+") {
-    return add(num1, num2);
-  } else if (operation == "-") {
-    return subtract(num1, num2);
-  } else if (operation == "*") {
-    return multiply(num1, num2);
-  } else if (operation == "/") {
-    return divide(num1, num2);
-  }
 }
 
 function refresh() {
   location.reload();
 }
 
-let num1 = "";
-let expression = "";
-const display = document.querySelector("#display");
-const fnum = document.querySelector("button");
+function num1() {
+  for (let i = 1; i <= 10; i++) {
+    const fnum = document.querySelector("#num" + i);
+    fnum.addEventListener("click", () => {
+      let num1 = "";
+      const display = document.querySelector("#display");
+      num1 = fnum.textContent;
+      display.textContent = display.textContent + Number(num1);
 
-fnum.addEventListener("click", () => {
-  num1 = fnum.textContent;
-  expression = expression + num1;
-  display.textContent = expression;
+      console.log(display.textContent)
+    });
+    console.log(display.textContent)
+  }
+}
 
-  console.log(num1);
-  console.log(expression);
-  console.log(display.textContent);
-})
+function expression() {
+  for (let i = 1; i <= 4; i++) {
+    let expression1 = "";
+    const display = document.querySelector("#display");
+    const fexpression = document.querySelector("#expression" + i);
 
+    fexpression.addEventListener("click", () => {
+      expression1 = fexpression.textContent;
+      display.textContent = display.textContent + expression1;
+    })
+  }
+}
+
+function num2() {
+  for (let i = 1; i <= 10; i++) {
+    let num2 = "";
+    const display = document.querySelector("#display");
+    const snum = document.querySelector("#num" + i);
+
+    snum.addEventListener("click", () => {
+      num2 = snum.textContent;
+      display.textContent = display.textContent + num2;
+    })
+  }
+}
 
 // let num1 = Number(prompt("Enter first number: "));
 // let num2 = Number(prompt("Enter second number: "));
 // let operation = prompt("Enter operation: ");
 
-// console.log(operate(num1, operation, num2));
+// console.log(num1());
+operate(num1(), expression(), num2());
